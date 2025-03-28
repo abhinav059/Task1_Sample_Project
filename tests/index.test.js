c2991b32f60e59f44dd2dc5101351971468d54b6
abhinav kumar <abhinavv0179@gmail.com>
2025-03-29 01:33:00 +0530

    changes in the test script

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,12 +1,13 @@
+
 const http = require('http');
-const request = require('supertest');
+const server = require('../index'); // Adjust path if necessary
 
-const server = require('../index'); // Ensure the path is correct
-
-describe('GET /', () => {
-  it('Server should return status code 200', (done) => {
-    request(server)
-      .get('/')
-      .expect(200, done);
+test('Server should return status code 200', (done) => {
+  const req = http.get('http://localhost:3000', (res) => {
+    expect(res.statusCode).toBe(200);
+    server.close(() => done()); // Close server and finish the test
+  });
+  req.on('error', (err) => {
+    server.close(() => done(err)); // Ensure server closes on error
   });
 });
